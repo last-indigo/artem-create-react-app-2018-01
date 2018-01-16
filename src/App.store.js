ef64bd50fe3a87aaf0bdcdb04d432499151cf723
Artem Drobotenko <Artem_Drobotenko@epam.com>
2018-01-16 19:15:14 +0200

    conenct time travel devtools

--- src/App.store.js
+++ src/App.store.js
@@ -5,4 +5,8 @@ export const preloadedState = {
     currentBalanceUSD: 0
 };
 
-export const myFirstStore = createStore(newRootReducerObjectCompositionDanAbramov, preloadedState, /* enhancerFn */);
+export const myFirstStore = createStore(
+    newRootReducerObjectCompositionDanAbramov,
+     preloadedState,
+     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      /* enhancerFn */);
